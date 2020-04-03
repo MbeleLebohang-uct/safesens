1e592af9bc0ed5fd50732635e25ec4789023568f
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-04-03 09:51:36 -0700

    Reverted the wsgi file

--- safesens/wsgi.py
+++ safesens/wsgi.py
@@ -1,13 +1,10 @@
 import os
 import sys
-
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safesens.settings')
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+application = Cling(get_wsgi_application())
