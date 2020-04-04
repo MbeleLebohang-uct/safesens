54d35382e353f9f6e2347718c580dbe37db778f8
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-04-04 16:29:10 +0200

    put back the amin url

--- safesens/settings.py
+++ safesens/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = get_list(
 # Application definition
 INSTALLED_APPS = [
     # Default apps
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- safesens/urls.py
+++ safesens/urls.py
@@ -1,11 +1,11 @@
 """
     safesens URL Configuration
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from safesens.pages.views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
