40595b7776f6b25c0f17164bdd5a3b01b7054020
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-04-04 13:58:22 +0200

    moved pages app inside the project

--- safesens/settings.py
+++ safesens/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # Custom apps
-    'pages',
+    'safesens.pages',
 ]
 
 MIDDLEWARE = [
--- safesens/urls.py
+++ safesens/urls.py
@@ -3,7 +3,8 @@
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from safesens.pages.views import home_view
+
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
