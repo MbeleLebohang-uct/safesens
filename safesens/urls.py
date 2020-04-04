67ef78102ab149e13044b3831359da8bf7552865
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-04-05 00:41:29 +0200

    added csrf exempt on the graphql endpoint

--- safesens/urls.py
+++ safesens/urls.py
@@ -5,10 +5,11 @@ from django.contrib import admin
 from django.urls import path
 from safesens.pages.views import home_view
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 from .schema import schema
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path("graphql/", GraphQLView.as_view(graphiql=True,schema=schema), name="graphql"),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True,schema=schema))),
     path('admin/', admin.site.urls),
 ]
