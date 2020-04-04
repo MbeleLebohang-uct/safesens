7c5a5f89f9b7218a3d287319266ab92dfafd30e0
Mbele Lebohang <mblleb006@myuct.ac.za>
2020-04-04 13:16:38 +0200

    changed home message

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Now it works</h1>")
+    return HttpResponse("<h1>API Documentation coming soon</h1>")
